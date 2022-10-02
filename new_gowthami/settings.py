2a2de0076490241e9225fd587c08d39daaf48b61
saikrupa82 <46783175+saikrupa82@users.noreply.github.com>
2022-10-02 13:43:44 +0530

    weblink

--- new_gowthami/settings.py
+++ new_gowthami/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vk^#m)sgb!dwlvsmi36^q1=0suf%xy!(le+=t-^69k#t(8h-co
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 WSGI_APPLICATION = 'gowthami.wsgi.application'
-ALLOWED_HOSTS = ["https://gowthami-website1.herokuapp.com/","http://127.0.0.1:8000/"]
+ALLOWED_HOSTS = ["https://gowthamidemo.herokuapp.com/","http://127.0.0.1:8000/"]
 
 # Application definition
 
